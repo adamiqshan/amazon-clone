ab254465c929760015da983247619393ba79459f
adam iqshan <omai.mohmed9@gmail.com>
2020-10-04 00:46:14 +0530

    nav bar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './Header';
 
 function App() {
   return (
@@ -9,12 +10,14 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/checkout">
+            <Header />
             <h1>Checkout</h1>
           </Route>
           <Route path="/login">
             <h1>Login</h1>
           </Route>
           <Router path="/">
+            <Header />
             <h1>Home Page</h1>
           </Router>
         </Switch>
---
+++ src/Header.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Header.css'
+import { Link } from 'react-router-dom';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+
+function Header() {
+    return (
+        <nav className="header">
+            <Link to="/">
+                <img className="header__logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" />
+            </Link>
+            <div className="header__search">
+                <input type="text" className="header__searchInput" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+            <div className="header__nav">
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span className="header__OptionLineOne">Hello Adam</span>
+                        <span className="header__OptionLineTwo">Sign In</span>
+                    </div>
+                </Link>
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span className="header__OptionLineOne">Returns</span>
+                        <span className="header__OptionLineTwo">& Orders</span>
+                    </div>
+                </Link>
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span className="header__OptionLineOne">Your</span>
+                        <span className="header__OptionLineTwo">PRIME</span>
+                    </div>
+                </Link>
+                <Link to="/checkout" className="header__link">
+                    <div className="header__optionBasket">
+                        <ShoppingCartIcon />
+                        <span className="header__OptionLineTwo header__basketCount">0</span>
+                    </div>
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
